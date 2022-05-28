5f56adc76a8851035864b6042d5057869ffeb710
Aidan Brook <aidanzhaobrook@gmail.com>
2022-05-28 13:11:00 -0400

    Create 256 Boxes To Make The Grid Area

---
+++ js-file.js
@@ -0,0 +1,12 @@
+const gridArea = document.querySelector(".container");
+let grid = [];
+
+for (i = 0; i < 256; i++) {
+  let box = document.createElement("div");
+  box.setAttribute(
+    "style",
+    "border: 1px solid black; height:60px; width:60px;"
+  );
+  gridArea.appendChild(box);
+  console.log(gridArea);
+}
