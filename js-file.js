16846db0d24d6d4f6cb85a992f8948f7197f5cfa
Aidan Brook <aidanzhaobrook@gmail.com>
2022-05-28 13:43:37 -0400

    Add Grid Sizing Button
    Users can click a button and be prompted to change the grid size.
    Function createGrid creates a grid based on the size input and the
    function changeSize prompts the user what size they want and calls
    createGrid to make the grid with the new size.

--- js-file.js
+++ js-file.js
@@ -1,12 +1,26 @@
 const gridArea = document.querySelector(".container");
-let grid = [];
+const button = document.querySelector("button");
 
-for (i = 0; i < 256; i++) {
-  let box = document.createElement("div");
-  box.setAttribute(
-    "style",
-    "border: 1px solid black; height:38px; width:38px;"
-  );
-  gridArea.appendChild(box);
-  console.log(gridArea);
+createGrid();
+
+button.addEventListener("click", changeSize);
+
+function changeSize() {
+  const size = prompt("What number of grid squares per side would you like?");
+  return createGrid(size);
+}
+
+function createGrid(size = 16) {
+  if (size > 100) {
+    return "ERROR GRID TOO LARGE";
+  } else {
+    for (i = 0; i < size ** 2; i++) {
+      let box = document.createElement("div");
+      box.setAttribute(
+        "style",
+        `border: 1px solid black; height:${38}px; width:${38}px;`
+      );
+      gridArea.appendChild(box);
+    }
+  }
 }
