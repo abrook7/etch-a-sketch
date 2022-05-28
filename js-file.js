63a023ea093a5e69969d8b3651aed4cd5e269165
Aidan Brook <aidanzhaobrook@gmail.com>
2022-05-28 13:28:02 -0400

    Align Div Boxes Into 16x16 Grid

--- js-file.js
+++ js-file.js
@@ -5,7 +5,7 @@ for (i = 0; i < 256; i++) {
   let box = document.createElement("div");
   box.setAttribute(
     "style",
-    "border: 1px solid black; height:60px; width:60px;"
+    "border: 1px solid black; height:38px; width:38px;"
   );
   gridArea.appendChild(box);
   console.log(gridArea);
